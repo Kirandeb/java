ffa7565d21a33fefc4fa6a090c1328767d694e18
Kirandeb <127499786+Kirandeb@users.noreply.github.com>
2023-03-18 07:41:48 +0530

    Add files via upload

---
+++ DateSizeRange.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+import java.util.*;
+class DateSizeRange
+{
+	public static void main(String args[])
+	{
+	 	System.out.println("Int Min : "+Integer.MIN_VALUE);  
+		System.out.println("Int MAX : "+Integer.MAX_VALUE);  
+		System.out.println("Int Bytes : "+Integer.SIZE);
+
+		System.out.println("Byte Min : "+Byte.MIN_VALUE);  
+		System.out.println("Byte MAX : "+Byte.MAX_VALUE);  
+		System.out.println("Byte Bytes : "+Byte.SIZE);
+
+		System.out.println("Float Min : "+Float.MIN_VALUE);  
+		System.out.println("Float MAX : "+Float.MAX_VALUE);  
+		System.out.println("Float Bytes : "+Float.SIZE);	
+	}
+}
+
+
+
